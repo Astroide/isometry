63b038d0ffc2248358903388304aea6aa5fea907
Olie Auger <olie.auger@gmail.com>
2021-12-07 18:42:44 -0500

    Sync !

--- script.js
+++ script.js
@@ -1,166 +1,147 @@
-/** @type {HTMLCanvasElement} */
-const canvas = document.querySelector("#canvas");
-/** @type {HTMLCanvasElement} */
-const targetCanvas = document.querySelector("#target");
-const ctx = canvas.getContext('2d');
-const targetCtx = targetCanvas.getContext('2d');
-const width = parseInt(prompt("Image Width"));
-const depth = parseInt(prompt("Image Height"));
+let currentColor = '#000000';
+function createColorElement(color) {
+  let colorElement = document.createElement('div');
+  colorElement.className = 'color';
+  colorElement.style.backgroundColor = color;
+  colorElement.addEventListener('click', () => {
+    setColor(color);
+  });
+  return colorElement;
+}
+function setColor(color, fromInput) {
+  currentColor = color;
+  if (!fromInput) {
+    document.querySelector('input[type=color]').value = currentColor;
+  } else {
+    document.querySelector('#colors').appendChild(createColorElement(currentColor));
+  }
+}
+function generatePng(element) {
+  let canvas = document.createElement('canvas');
+  canvas.width = layers[0].width;
+  canvas.height = layers[0].height + (layers.length - 1);
+  document.body.appendChild(canvas);
+  let ctx = canvas.getContext('2d');
+  for (let i = 0; i < layers.length; i++) {
+    for (let x = 0; x < layers[i].width; x++) {
+      for (let y = 0; y < layers[i].height; y++) {
+        if (layers[i].colorData[y][x]) {
+          let color = (layers[i].colorData[y][x] || '#000000') + (layers[i].colorData[y][x] ? 'ff' : '00');
+          let r = parseInt(color.slice(1, 3), 16);
+          let g = parseInt(color.slice(3, 5), 16);
+          let b = parseInt(color.slice(5, 7), 16);
+          let a = parseInt(color.slice(7, 9), 16);
+          ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a / 255})`;
+          ctx.fillRect(x, y + (layers.length - 1) - i, 1, 1);
+          // console.log(r, g, b);
+          // setPixel(data, x, y + (layers.length - 1) - i, r, g, b, a);
+        }
+      }
+    }
+  }
+  // ctx.putImageData(data, 0, 0);
+  element.download = 'image.png';
+  element.href = canvas.toDataURL('image/png');
+  canvas.remove();
+}
+function generateDmPng(element) {
+  let canvas = document.createElement('canvas');
+  canvas.width = layers[0].width;
+  canvas.height = layers[0].height + (layers.length - 1);
+  document.body.appendChild(canvas);
+  let ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < layers.length; i++) {
+    for (let x = 0; x < layers[i].width; x++) {
+      for (let y = 0; y < layers[i].height; y++) {
+        if (layers[i].colorData[y][x]) {
+          let layerHeight = ((layers.length - 1) - i) - layers[i].height;
+          let depth = layerHeight + (layers[i].height - y) + 128;
+          ctx.fillStyle = `rgba(${depth}, ${depth}, ${depth}, 1)`;
+          ctx.fillRect(x, y + (layers.length - 1) - i, 1, 1);
+          // console.log(r, g, b);
+          // setPixel(data, x, y + (layers.length - 1) - i, r, g, b, a);
+        }
+      }
+    }
+  }
+  // ctx.putImageData(data, 0, 0);
+  element.download = 'image.dm.png';
+  element.href = canvas.toDataURL('image/png');
+  canvas.remove();
+}
 class Layer {
-  constructor(width, height) {
+  constructor(width, height, id, maxLayers) {
     this.width = width;
     this.height = height;
     this.colorData = [];
     for (let i = 0; i < width; i++) {
-      this.colorData.push(new Int32Array(height));
+      this.colorData.push(new Array(height));
       for (let j = 0; j < height; j++) {
-        this.colorData[i][j] = Math.floor(Math.random() * 0xFFFFFF);
+        this.colorData[i][j] = null;
       }
     }
-    this.depthMapData = [];
-    for (let i = 0; i < width; i++) {
-      this.depthMapData.push(new Int32Array(height));
-      for (let j = 0; j < height; j++) {
-        this.depthMapData[i][j] = 128;
-      }
-    }
-    this.active = true;
-    this.important = false;
-    this.useDepthMap = true;
-  }
-
-  setImportant(value) {
-    this.important = value;
-    
-  }
-}
-class IsometricRenderer {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.data = new Int32Array(width * height);
-    this.depth = new Int32Array(width * height);
-    this.whatWasDrawn = new Array(width * height);
-    this.noOverride = new Array(width * height);
-
-    for (let i = 0; i < this.noOverride.length; i++) {
-      this.noOverride[i] = false;
-    }
-    this.setTranslation(0, 0);
-    this.setAdditionalTranslation(0, 0);
-  }
-
-  resize(newWidth, newHeight) {
-    if (this.width == newWidth && this.height == newHeight) {
-      return;
-    }
-    this.width = newWidth;
-    this.height = newHeight;
-    this.data = new Int32Array(width * height);
-    this.depth = new Int32Array(width * height);
-    this.whatWasDrawn = new Array(width * height);
-    this.noOverride = new Array(width * height);
-
-    for (let i = 0; i < this.noOverride.length; i++) {
-      this.noOverride[i] = false;
-    }
-  }
-
-  setTranslation(x, y) {
-    this.xOffset = x;
-    this.yOffset = y;
-  }
-
-  setAdditionalTranslation(x, y) {
-    this.xTranslation = x;
-    this.yTranslation = y;
-  }
-
-  clear() {
-    for (let i = 0; i < this.data.length; i++) {
-      this.data[i] = 0;
-    }
-    for (let i = 0; i < this.depth.length; i++) {
-      this.depth[i] = -2147483648;
-    }
-    for (let i = 0; i < this.whatWasDrawn.length; i++) {
-      this.whatWasDrawn[i] = 0;
-    }
-    for (let i = 0; i < this.noOverride.length; i++) {
-      this.noOverride[i] = false;
-    }
-  }
-
-  transformPoint(x, y, z) {
-    let targetX = (x - z);
-    let targetY = -(x + z) / 2 - y;
-    return [targetX, targetY];
-  }
-
-  drawImage(image, x, y, z, id) {
-    let targetX = (x - z);
-    let targetY = -(x + z) / 2 - y;
-    let baseDepth = -(x + z) / 2 + y;
-    let rx = targetX + (this.xOffset + this.xTranslation) - image.width / 2;
-    let ry = targetY + (this.yOffset + this.yTranslation) - image.height;
-    if (rx >= this.width || ry >= this.height || (rx + image.width) < 0 || (ry + image.height) < 0) {
-      return;
-    }
-    for (let i = 0; i < image.width; i++) {
-      for (let j = 0; j < image.height; j++) {
-        let tx = targetX + i - image.width / 2 + (this.xOffset + this.xTranslation);
-        let ty = targetY + j - image.height + (this.yOffset + this.yTranslation);
-        if (tx < 0 || ty < 0 || tx >= this.width || ty >= this.height) {
-          continue;
-        }
-        let index = (ty * this.width + tx);
-        let localDepth = image.useDepthMap ? (baseDepth + (image.depthMapData[i][j] - 128)) : baseDepth;
-        if (this.depth[index] <= localDepth) {
-          if ((!image.useDepthMap || image.depthMapData[i][j] != 0)) {
-            this.depth[index] = localDepth;
-            this.data[index] = image.colorData[i][j];
-            if (!this.noOverride[index]) {
-              this.whatWasDrawn[index] = id;
-            }
+    const layerElement = document.createElement('div');
+    layerElement.className = 'layer';
+    layerElement.style.zIndex = id * 10;
+    for (let i = 0; i < height; i++) {
+      const row = document.createElement('div');
+      row.className = 'row';
+      for (let j = 0; j < width; j++) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.style.zIndex = id * 10;
+        let self = this;
+        cell.addEventListener('click', () => {
+          if (cell.parentElement.parentElement.classList.contains('active')) {
+            let x = i;
+            let y = j;
+            cell.style.backgroundColor = currentColor;
+            self.colorData[x][y] = currentColor;
           }
-        } else if (image.depthMapData[i][j] == 255) {
-          this.noOverride[index] = true;
-          this.whatWasDrawn[index] = id;
-        }
+        });
+        row.appendChild(cell);
       }
+      layerElement.appendChild(row);
     }
+    const numberElement = document.createElement('div');
+    this.numberElement = numberElement;
+    numberElement.className = 'layer-number';
+    numberElement.innerText = id;
+    numberElement.style.top = `${(maxLayers - id) * 20 + 40}px`;
+    let layer = this;
+    numberElement.addEventListener('click', () => {
+      layer.setActive(true);
+    });
+    document.querySelector('#layers').appendChild(numberElement);
+    layerElement.style.left = '40px';
+    layerElement.style.top = `${(maxLayers - id) * 20 + 40}px`;
+    this.element = layerElement;
+    document.querySelector('#layers').appendChild(layerElement);
   }
 
-  copyToCanvas(ctx) {
-    const imageData = ctx.getImageData(0, 0, this.width, this.height);
-    for (let i = 0; i < this.data.length; i++) {
-      imageData.data[i * 4] = (this.data[i] & 0xFF0000) >> 16;
-      imageData.data[i * 4 + 1] = (this.data[i] & 0x00FF00) >> 8;
-      imageData.data[i * 4 + 2] = (this.data[i] & 0x0000FF);
-      imageData.data[i * 4 + 3] = 255;
+  setActive(value) {
+    if (value) {
+      document.querySelectorAll('.layer').forEach(layer => {
+        layer.classList.remove('active');
+      });
+      document.querySelectorAll('.layer-number').forEach(layer => {
+        layer.classList.remove('active');
+      });
+      this.element.classList.add('active');
+      this.numberElement.classList.add('active');
+    } else {
+      this.element.classList.remove('active');
+      this.numberElement.classList.remove('active');
     }
-    ctx.putImageData(imageData, 0, 0);
-  }
-
-  idAt(x, y) {
-    return this.whatWasDrawn[y * this.width + x];
   }
 }
 let layers = [];
-layers.push(new Layer(width, depth));
-let renderer = new IsometricRenderer(100, 100);
-function render() {
-  renderer.clear();
-  renderer.setTranslation(50, 100);
-  for (let i = 0; i < layers.length; i++) {
-    if (layers[i].active) {
-      renderer.drawImage(layers[i], 0, i, 0, 0);
-    }
-  }
-  targetCtx.clearRect(0, 0, 100, 100);
-  renderer.copyToCanvas(targetCtx);
-  ctx.clearRect(0, 0, 200, 200);
-  ctx.imageSmoothingEnabled = false;
-  ctx.drawImage(targetCanvas, 0, 0, 400, 400);
+const width = parseInt(prompt('width'));
+const height = parseInt(prompt('height'));
+const depth = parseInt(prompt('depth'));
+for (let i = 0; i < height; i++) {
+  layers.push(new Layer(width, depth, i, height));
 }
-render();
+layers[0].setActive(true);
